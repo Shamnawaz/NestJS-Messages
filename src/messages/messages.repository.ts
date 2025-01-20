1d9d8c3202e4eafeecb8b75141bb60fe019b9a9c
Shamnawaz <shamnawaz.subakhan.sio@gmail.com>
2025-01-20 09:19:38 +0100

    Reporting Errors

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -22,7 +22,6 @@ export class MessagesRepository {
 
         messages[id] = { id, content };
 
-        await writeFile('message.json', JSON.stringify(messages)); 
+        await writeFile('messages.json', JSON.stringify(messages)); 
     }
-
 }
